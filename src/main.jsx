7affeeca9ab96698a4aebdb9d18f9f3645dbc3b4
Zak <119068067+zulfiqarak7@users.noreply.github.com>
2026-03-22 19:09:40 -0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,3 +1,8 @@
+import './index.css' // This connects your CSS to the app
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+// ... rest of the file
 import React, { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
