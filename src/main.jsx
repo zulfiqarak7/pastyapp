6c50cb5b7956b40eb51ff0f00443f7400b2dc764
Zak <119068067+zulfiqarak7@users.noreply.github.com>
2026-03-22 19:20:29 -0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,10 @@
-import './index.css' // This connects your CSS to the app
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-// ... rest of the file
-import React, { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
     <App />
-  </StrictMode>
+  </React.StrictMode>,
 )
